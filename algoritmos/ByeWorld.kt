7dfdcf073965960885c73db886e1403baedc994d
stikrobinson <148508110+stikrobinson@users.noreply.github.com>
2025-11-04 02:58:25 +0000

    avance 1 y algoritmo byeworld

---
+++ algoritmos/ByeWorld.kt
@@ -0,0 +1,28 @@
+/*
+Prueba hecha por Steve Robinson
+Lenguaje de programación de Kotlin
+*/
+fun funcion() {
+    var hola : Boolean = true  
+    
+    // Comentario
+    if( 1 >= 10+10 && 10 < 10-9 || 9*2 == 10/11 && 10 != 9 || 10<=9 && false || 9>0){
+        print("
+              hola
+        ");
+    }
+    
+    while(hola){
+        var numero : Double = 10.05
+        val mutableList: MutableList<Long> = mutableListOf(12, 10, 20)
+        
+        for(elemento in mutableList){
+			val myMutableMap<String, Long> = mutableMapOf("one" to 1, "two" to 2)
+            print(elemento)
+        }
+        
+        hola = !hola
+        var bool: Boolean = (hola && hola) || hola
+       
+    }
+}
--- codigo/Analizador_Lexico.py
+++ codigo/Analizador_Lexico.py
@@ -1,20 +1,19 @@
+from zoneinfo import ZoneInfo
 import ply.lex as lex
+from datetime import datetime
+import os
 
 # Inicio del avance 1 por parte de Jefferson Saltos
 reserved = {
    "fun": "FUNCTION",
    'if' : 'IF',
-   'then' : 'THEN',
    'else' : 'ELSE',
    'while' : 'WHILE',
    'for' : 'FOR',
    'val' : 'VAL',
    'var' : 'VAR',
-   'Long': 'LONG',
-   'Double': 'DOUBLE',
    'in' : 'IN',
    'return' : 'RETURN',
-
 }
 
 # List of token names.   This is always required
@@ -33,7 +32,7 @@ tokens = (
    'RBRACE',
    'LBRACKET',
    'RBRACKET',
-   'VARIABLE',
+   'IDENTIFIER',
    'LESS_THAN',
    'BOOLEAN',
    "GREATER_THAN",
@@ -50,12 +49,15 @@ tokens = (
    "EQUALS_TO",
    "NOT_EQUALS",
    "COMMENT_ONE_LINE",
-   "COMMENT_MULTIPLE_LINES"
-
+   "COMMENT_MULTIPLE_LINES",
+   "RANGE",
+   "COMMA"
 ) + tuple(reserved.values())
 
 # Regular expression rules for simple tokens
+t_COMMA = r","
 t_COLON= r':'
+t_RANGE = r"\.\."
 t_SEMICOLON= r';'
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
@@ -74,10 +76,8 @@ t_AND=r'&&'
 t_OR=r'\|\|'
 t_NOT=r'!'
 t_ASSIGMENT=r'='
-t_COMMENT_ONE_LINE=r'\/\/[\w ]*'
-t_COMMENT_MULTIPLE_LINES=r'\/\*(.|\s)*\*\/'
-
-
+t_COMMENT_ONE_LINE=r'\/\/.*'
+t_COMMENT_MULTIPLE_LINES=r'\/\*[^\*\/]*\*\/'
 
 def t_MUTABLE_LIST(t):
     r'MutableList<.+>'
@@ -88,3 +88,116 @@ def t_MUTABLE_LIST_ERROR(t):
     print(f"Error léxico: MutableList mal formado '{t.value}' en línea {t.lineno}, posición {t.lexpos}")
     
 #Fin de avance 1 por parte de Jefferson Saltos
+
+#Inicio de avance 1 por parte de Steve Robinson
+
+def t_MUTABLE_MAP(t):
+    r'MutableMap<.+,.+>'
+    return t
+
+def t_MUTABLE_MAP_ERROR(t):
+    r'MutableMap<\s*>|MutableMap<.+>|MutableMap\s*<|MutableMap[^<]'
+    print(f"Error léxico: MutableMap mal formado '{t.value}' en línea {t.lineno}, posición {t.lexpos}")
+    return f"Error léxico: MutableMap mal formado '{t.value}' en línea {t.lineno}, posición {t.lexpos}"
+
+def t_GREATER_OR_EQUAL(t):
+    r'>='
+    return t
+
+def t_LESS_OR_EQUAL(t):
+    r'<='
+    return t
+
+def t_EQUALS_TO(t):
+    r'=='
+    return t
+
+def t_NOT_EQUALS(t):
+    r'!='
+    return t
+
+def t_BOOLEAN(t):
+    r'(true|false)'
+    return t
+
+def t_STRING(t):
+    #r'"[.\s]*"'
+    r'"[^"]*"'
+    t.value=t.value[1:-1]
+    return t
+
+def t_IDENTIFIER(t):
+    r'[A-Za-z$_][\w$]*'
+    t.type= reserved.get(t.value,"IDENTIFIER")
+    return t
+
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    t.value=float(t.value)
+    return t
+
+# A regular expression rule with some action code
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print(f"Caracter lexico no existente '{t.value[0]}' en el lenguaje Kotlin en línea {t.lineno}, posición {t.lexpos}")
+    t.lexer.skip(1)
+    return f"Caracter lexico no existente '{t.value[0]}' en el lenguaje Kotlin en línea {t.lineno}, posición {t.lexpos}"
+
+# Build the lexer
+lexer = lex.lex()
+
+with open('algoritmos/ByeWorld.kt', 'r') as file:
+    all_lines = file.readlines()
+    data = "".join(all_lines)
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+
+tokens_reconocidos = []
+errores = []
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    if isinstance(tok, str):  # error
+        errores.append(tok)
+    else:
+        tokens_reconocidos.append(f"{tok.type}: {tok.value}")
+
+# === Generar nombre de archivo ===
+usuario_git = "stikrobinson"
+ecuador_tz = ZoneInfo("America/Guayaquil")
+ahora = datetime.now(ecuador_tz)
+nombre_archivo = f"lexico-{usuario_git}-{ahora.strftime('%d-%m-%Y-%Hh%M')}.txt"
+ruta = f"logs/{nombre_archivo}"
+
+# === Escribir log ===
+with open(ruta, "w", encoding="utf-8") as f:
+    f.write("TOKENS RECONOCIDOS:\n")
+    for token in tokens_reconocidos:
+        f.write(token + "\n")
+    if len(errores) != 0:
+        f.write("\nERRORES:\n")
+        for error in errores:
+            f.write(error + "\n")
+
+
+print(f"Log generado: {ruta}")
+
+#Fin de avance 1 por parte de Steve Robinson
